a74c3563e56b033cb403ddd535f015b58faad43c
Zayra Ramos <zayraramos124@gmail.com>
2026-03-30 15:15:42 -0500

    corrijo logos

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,13 +18,12 @@ export const metadata = {
   },
 
   icons: {
-    icon: [
-      { url: '/icon-light-32x32.png', media: '(prefers-color-scheme: light)' },
-      { url: '/icon-dark-32x32.png', media: '(prefers-color-scheme: dark)' },
-      { url: '/icon.svg', type: 'image/svg+xml' },
-    ],
-    apple: '/apple-icon.png', // 👈 CLAVE
-  },
+  icon: [
+    { url: '/icon-192.png', sizes: '192x192', type: 'image/png' }, // 👈 CLAVE
+    { url: '/icon-512.png', sizes: '512x512', type: 'image/png' }, // 👈 CLAVE
+  ],
+  apple: '/apple-icon.png',
+},
 }
 export const viewport = {
   themeColor: '#1e3a5f',
